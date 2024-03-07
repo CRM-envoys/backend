6d689637b46046b497489be582c9f4028ace01c4
Raskatov Andrey <btrclsl@gmail.com>
2024-03-07 18:01:05 +1000

    fix: небольшое исправление

--- crm_yandex/crm_yandex/settings.py
+++ crm_yandex/crm_yandex/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = "crm_yandex.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("POSTGRES_DB"),
+        "USER": os.getenv("POSTGRES_USER"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
