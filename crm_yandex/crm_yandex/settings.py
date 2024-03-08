bcc6139b4e3c057d8631adef43ab9b2c2d0b850f
Serhii Vinogradov <123404466+yan-gabala@users.noreply.github.com>
2024-03-08 12:37:27 +0300

    feature: Добавил скрипт деплоя, в конфиг новый сервер

--- crm_yandex/crm_yandex/settings.py
+++ crm_yandex/crm_yandex/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1", "localhost", "158.160.13.137", "crm-yandex.ddns.net"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "92.63.105.199", "devinse.store"]
 
 
 # Application definition
