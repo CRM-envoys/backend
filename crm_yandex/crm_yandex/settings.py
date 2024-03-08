e919ca841056f719907c7a865279a5bdb1d8d153
Serhii Vinogradov <123404466+yan-gabala@users.noreply.github.com>
2024-03-08 15:24:58 +0300

    bug-fix: Иное описание секретов для постгрес

--- crm_yandex/crm_yandex/settings.py
+++ crm_yandex/crm_yandex/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = "crm_yandex.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
+"""
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
@@ -92,6 +92,18 @@ DATABASES = {
     }
 }
 
+"""
+
+DATABASES = {
+    "default": {
+        "ENGINE": os.getenv("DB_ENGINE", default="django.db.backends.postgresql"),
+        "NAME": os.getenv("DB_NAME", default="postgres"),
+        "USER": os.getenv("POSTGRES_USER", default="postgres"),
+        "PASSWORD": os.getenv("POSTGRES_PASSWORD", default="postgres"),
+        "HOST": os.getenv("DB_HOST", default="db"),
+        "PORT": os.getenv("DB_PORT", default="5432"),
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
