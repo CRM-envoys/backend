d7f380aa8b527ba44e21d9a7b0302b9ac681ffda
Serhii Vinogradov <123404466+yan-gabala@users.noreply.github.com>
2024-03-11 10:04:37 +0300

    ci: Перезапуск сервера

--- crm_yandex/crm_yandex/settings.py
+++ crm_yandex/crm_yandex/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-ir8*tzr*o!cqe@h)a35pht8($4*-+rqn6qp$f0g$*o9+g(a79n")
+SECRET_KEY = "django-insecure-ir8*tzr*o!cqe@h)a35pht8($4*-+rqn6qp$f0g$*o9+g(a79n"
+# SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-ir8*tzr*o!cqe@h)a35pht8($4*-+rqn6qp$f0g$*o9+g(a79n")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
